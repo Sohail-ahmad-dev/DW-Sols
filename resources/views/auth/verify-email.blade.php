6a5ffcf337e079eab4cd0abe7396bb015dd93c1b
Sohail-ahmad-dev <107072917+Sohail-ahmad-dev@users.noreply.github.com>
2023-03-15 10:21:21 +0500

    email verify

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,40 +1,33 @@
-@if(empty(Auth::user()->email_verified_at))
-  
-  
-    @section('content')
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+@extends('layouts.app')
 
-                        <div class="card-body">
-                            {{-- @if (session('resent'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ __('A fresh verification link has been sent to your email address.') }}
-                                </div>
-                            @endif --}}
-                            @if (session('message'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ __('A fresh verification link has been sent to your email address.') }}
-                                </div>
-                            @endif
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
-                            {{ __('Before proceeding, please check your email for a verification link.') }}
-                            {{ __('If you did not receive the email') }},
-                            <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
-                                @csrf
-                                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                            </form>
-                        </div>
+                    <div class="card-body">
+                        {{-- @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('A fresh verification link has been sent to your email address.') }}
+                            </div>
+                        @endif --}}
+                        @if (session('message'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('A fresh verification link has been sent to your email address.') }}
+                            </div>
+                        @endif
+
+                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                        {{ __('If you did not receive the email') }},
+                        <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
+                            @csrf
+                            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    @endsection
-  
-  
-@else 
-  <script>window.location = "/home";</script>
-@endif
-@extends('layouts.app')
+    </div>
+@endsection
